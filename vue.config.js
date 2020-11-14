a96ee28df24b2d9ebc99d8051cac60a73c115a42
dengdengdengpan <1306631506@qq.com>
2020-11-14 21:39:44 +0800

    添加 svgo-loader

--- vue.config.js
+++ vue.config.js
@@ -4,11 +4,14 @@ const path = require('path')
 module.exports = {
   chainWebpack: config => {
     const dir = path.resolve(__dirname, 'src/icons/svg')
+    const svgoConfig = path.resolve(__dirname, 'src/icons/svgo.yml')
+
     config.module
       .rule('svg-sprite')
       .test(/\.svg$/)
       .include.add(dir).end()
       .use('svg-sprite-loader').loader('svg-sprite-loader').options({ extract: false }).end()
+      .use('svgo-loader').loader('svgo-loader').options({ externalConfig: svgoConfig }).end()
     config.plugin('svg-sprite').use('svg-sprite-loader/plugin', [{ plainSprite: true }])
     config.module.rule('svg').exclude.add(dir)  
   }
