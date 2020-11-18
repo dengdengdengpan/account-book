abbf00c0484b9b784b33b606bc20190b3e3f0e82
dengdengdengpan <1306631506@qq.com>
2020-11-18 23:39:01 +0800

    添加三个 SVG 图标

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,15 +1,33 @@
 <template>
   <layout>
-    <p>记账</p>
+    <div class="account-wrapper">
+      <div class="account-container">
+        <div class="type">记账类型</div>
+        <div class="money">记账金额</div>
+        <ul class="label-list">记账标签列表</ul>
+        <div class="remark">备注</div>
+        <ul class="digital-panel">数字面板</ul>
+      </div>
+    </div>
   </layout>
 </template>
 
-<script>
+<script lang="ts">
 export default {
   name: 'Detail'
 }
 </script>
 
 <style lang="scss" scoped>
+.account-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  height: 100%;
+  background-color: rgba(105, 106, 107, .2);
 
+  .account-container {
+    background-color: pink;
+  }
+}
 </style>
